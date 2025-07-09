23f224c379ffcc72006a6ddc4cf6c7b893d99eae
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2025-07-09 05:57:01 -0600

    [FIX] sale: remove analytic_distribution as protected field in sale order line

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -1064,7 +1064,7 @@ class SaleOrderLine(models.Model):
         """
         return [
             'product_id', 'name', 'price_unit', 'product_uom', 'product_uom_qty',
-            'tax_id', 'analytic_distribution'
+            'tax_id'
         ]
 
     def _update_line_quantity(self, values):
